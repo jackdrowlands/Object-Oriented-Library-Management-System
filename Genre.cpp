72b698bf3f5164f6b7fa24ed06f0f595cd841376
Isaac Hampel <isaachampel@Isaacs-MacBook-Air-2.local>
2023-10-16 15:19:33 +1030

    Genre getters/setters

--- Genre.cpp
+++ Genre.cpp
@@ -9,3 +9,24 @@ Genre::Genre(int id, std::string name, std::vector<Book> books, bool restricted,
   this->books = books;
 }
 // Add other method implementations here, if any
+
+// Setters and getters
+  std::vector<Book> Genre::getBooks(){
+    return books;
+  }
+  bool Genre::isRestricted(){
+    return restricted;
+  }
+  bool Genre::isFictional(){
+    return fictional;
+  }
+
+  void Genre::setBooks(std::vector<Book> books){
+    this->books = books;
+  }
+  void Genre::setRestricted(bool restricted){
+    this->restricted = restricted;
+  }
+  void Genre::setFictional(bool fictional){
+    this->fictional = fictional;
+  }
